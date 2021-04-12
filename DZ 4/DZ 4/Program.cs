09505f2ef972be85021cb2e092f18da87e25d5ee
Galina2506 <galya.voytovich@mail.ru>
2021-04-12 20:57:38 +0300

    ДЗ 4

---
+++ DZ 4/DZ 4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DZ_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetFullName("Тучин", "Роман", "Сергеевич"));
+            Console.WriteLine(GetFullName("Куликова", "Ирина", "Вячславовна"));
+            Console.WriteLine(GetFullName("Литвинова", "Варвара", "Александровна"));
+            Console.WriteLine(GetFullName("Сычева", "Анна", "Владиславовна"));
+            Console.WriteLine();
+
+
+
+            Console.Write("Введите цифры через пробел: ");
+            Console.WriteLine(GetSum(Console.ReadLine()));
+            Console.WriteLine();
+
+        }
+    }
+}
